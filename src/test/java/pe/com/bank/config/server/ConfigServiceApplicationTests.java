8127c005910e2a1cf4796d0ac9c4c80ea2e8d159
Edwin Ruben Rea Morales <ereamora@emeal.nttdata.com>
2022-05-31 12:19:27 -0500

    first commit config server service

---
+++ src/test/java/pe/com/bank/config/server/ConfigServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package pe.com.bank.config.server;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 1);
+ }
+}
